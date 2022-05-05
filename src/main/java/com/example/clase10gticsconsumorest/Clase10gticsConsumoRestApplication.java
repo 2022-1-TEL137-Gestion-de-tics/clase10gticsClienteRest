7cc3ba7755ab942f0b1a133f95ae621e3d2cd209
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2022-05-05 13:22:10 -0500

    proyecto base

---
+++ src/main/java/com/example/clase10gticsconsumorest/Clase10gticsConsumoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase10gticsconsumorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase10gticsConsumoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase10gticsConsumoRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clase10gticsconsumorest/Clase10gticsConsumoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase10gticsconsumorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase10gticsConsumoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
